f962f3cc437ab1e242f7788534430ac103a249bb
xmmm <xmmm>
2020-01-06 11:28:17 +0800

    fix

--- src/my/Chain/Handler.java
+++ src/my/Chain/Handler.java
@@ -7,24 +7,28 @@ import java.util.ArrayList;
 
 /**
  * 责任链设计模式
- * 
+ * <p>
  * 处理逻辑操作
- * 
+ * <p>
  * 提供逻辑操作处理的抽象方法 设置下一级操作者的方法
- * 
- * @author hmj
  *
+ * @author hmj
  */
 public abstract class Handler {
-	/**责任下一级处理人 */
-	protected Handler next;
+    /**
+     * 责任下一级处理人
+     */
+    protected Handler next;
+    private String a = "ssss";
+
     //设置下一级责任链
-    public void setSuccessor(Handler handler){
-        this.next=handler;
+    public void setSuccessor(Handler handler) {
+        this.next = handler;
     }
-    
+
     public Handler getNext() {
         return this.next;
     }
-    public abstract ArrayList<String> requestFilename(String filename,String vague, String index,String root,String type);
+
+    public abstract ArrayList<String> requestFilename(String filename, String vague, String index, String root, String type);
 }
